926442b004d224fd8f70c1ce8550e9134ba2ccbd
TebogoNkomondo <1124458@students.wits.ac.za>
2020-05-02 20:33:05 +0200

    favorable form

--- Tweepy.py
+++ Tweepy.py
@@ -4,36 +4,8 @@ from tweepy import Stream
 from tweepy import API
 from tweepy import Cursor
 
+
 import credentials
-
-
-class TwitterClient():
-    def __init__(self, twitter_user=None):
-        self.auth = TwitterAuthenticator().authenticate_twitter_app()
-        self.twitter_client = API(self.auth)
-        
-        self.twitter_user = twitter_user
-        
-    def get_user_timeline_tweets(self, num_tweet):
-        tweets = []
-        for tweet in Cursor(self.twitter_client.user_timeline,id=self.twitter_user).items(num_tweet):
-            tweets.append(tweet)
-        return tweets
-    
-    def get_friend_list(self, num_friends):
-        friend_list =[]
-        for friend in Cursor(self.twitter_client.friends,id=self.twitter_user).items(num_friends):
-            friend_list.append(friend)
-        return friend_list
-    
-    def get_home_timeline_tweets(self, num_tweets):
-        home_timeline_tweets =[]
-        for tweet in Cursor(self.twitter_client.home_timeline,id=self.twitter_user).items(num_tweets):
-            home_timeline_tweets.append(tweet)
-        return home_timeline_tweets
-        
-        
-
 
 class TwitterAuthenticator():
     def authenticate_twitter_app(self):
@@ -78,8 +50,6 @@ if __name__ == "__main__":
     mylist =['hazard','fire','ablaze','accident','aftershock','ambulance']   
     fetched_tweets_filename = "text.txt"
     
-    twitter_client = TwitterClient('pycon')
-    print(twitter_client.get_user_timeline_tweets(5))
-    # tweeter_streamer = TweeterStreamer()
-    # tweeter_streamer.stream_tweets(fetched_tweets_filename,mylist)
+    tweeter_streamer = TweeterStreamer()
+    tweeter_streamer.stream_tweets(fetched_tweets_filename,mylist)
     
